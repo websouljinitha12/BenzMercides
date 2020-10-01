fb4a3acae01a6fe705256bc5f79456c1fc6f272c
websouljinitha12 <websoullabsjinithajoy@gmail.com>
2020-10-01 10:56:23 +0530

    fitst commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {
+    path: 'header', component: HeaderComponent
+  },
+
+  {
+    path: 'footer', component: FooterComponent
+  },
+  {
+    path: 'home', component: HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
